6368cf54367362b9714b259c0f109f7852ec5189
P.Sreeja <psreeja28@gmail.com>
2017-09-27 08:40:51 +0530

    Update the github package name and folders

---
+++ src/main/java/com/rubicon/rms/system/RestaurantSystem.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rubicon.rms.system;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Pakeeru Sreeja
+ */
+public class RestaurantSystem extends JFrame{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Login login = new Login();
+        login.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        login.setVisible(true);
+    }
+}
